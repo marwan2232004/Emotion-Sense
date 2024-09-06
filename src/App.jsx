1a8a84482ffbd0a036632756060a9f47534cb452
marwan2232004 <118024824+marwan2232004@users.noreply.github.com>
2024-09-06 10:52:45 +0300

    deploying server

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
   const handelClick = async () => {
     console.log(JSON.stringify({ text }));
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://emotionsense-dvemggcjagcqdxey.uaenorth-01.azurewebsites.net/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
